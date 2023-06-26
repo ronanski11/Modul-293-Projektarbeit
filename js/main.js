1021b2db5dccfbb7666b3f7d9956a3acd550f599
ronanski11 <ronanski11@outlook.com>
2023-06-26 15:45:21 +0200

    v 3.0

--- js/main.js
+++ js/main.js
@@ -24,3 +24,28 @@ document
     var dropdownMenu = document.getElementById("dropdown-menu");
     dropdownMenu.style.display = "none";
   });
+
+function formSubmit() {
+  alert("Form successfully submitted!");
+}
+function kontaktSubmit() {
+  var firstname = document.getElementById("firstname").value;
+  var lastname = document.getElementById("lastname").value;
+  var experience = document.getElementById("experience").value;
+
+  var emailBody =
+    "Vorname: " +
+    firstname +
+    "\n" +
+    "Nachname: " +
+    lastname +
+    "\n" +
+    "Nachricht: " +
+    experience;
+
+  var mailtoLink =
+    "mailto:ronan.coughlan@bbzbl-it.ch?subject=Kontakt Formular&body=" +
+    encodeURIComponent(emailBody);
+
+  window.open(mailtoLink, "_blank");
+}
